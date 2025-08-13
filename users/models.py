5ea31e0402c63a49d4bcbc411556fdcad6a15fb1
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-13 22:09:59 +0600

    initial project set-up done

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+from .managers import CustomUserManager
+
+
+class User( AbstractUser ):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    membership_date = models.DateField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
