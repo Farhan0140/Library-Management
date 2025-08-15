65ca2aa3d2f58413712fee284da765a6acd2c212
Farhan Nadim <farhannadim2023@gmail.com>
2025-08-15 16:33:36 +0600

    Enhance API and models: Add nested routing for categories and authors, implement BorrowRecord serializer with validation, and fix related name typo in Book model.

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path, include
 
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-from book.views import Book_View_Set
+from book.views import Book_View_Set, BorrowRecord_View_Set, Category_View_Set, Author_View_Set
 from users.views import Member_View_Set
 
 
-router = DefaultRouter()
-router.register('books', Book_View_Set)
-router.register('users', Member_View_Set)
+router = routers.DefaultRouter()
+router.register('books', Book_View_Set, basename='books')
+router.register('members', Member_View_Set, basename='members')
+router.register('borrow-record', BorrowRecord_View_Set)
+router.register('categories', Category_View_Set, basename='category')
+router.register('author', Author_View_Set, basename='author')
+
+
+category_router = routers.NestedDefaultRouter(router, 'categories', lookup='category')
+category_router.register('books', Book_View_Set, basename='category-books')
+
+author_router = routers.NestedDefaultRouter(router, 'author', lookup='author')
+author_router.register('books', Book_View_Set, basename='author-books')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(category_router.urls)),
+    path('', include(author_router.urls)),
 ]
